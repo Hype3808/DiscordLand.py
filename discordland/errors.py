5e4df0a101a829326fd927e619cb89dd7d46a897
Hype6969 <74125180+Hype3808@users.noreply.github.com>
2022-01-08 16:42:57 +0800

    Added `__all__`

--- discordland/errors.py
+++ discordland/errors.py
@@ -22,6 +22,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+__all__ = (
+    'BaseException',
+    'APIError'
+)
+
 class BaseException(Exception):
     """Base exception of discordland wrapper"""
 
